e12b8c6b9f2bb56ebdd06737fec9fd6be3f6009d
Nathan <64827034+nathansmith17@users.noreply.github.com>
2021-06-21 12:18:57 +1200

    Create 01_helpguiV2

--- versions/01_help_guiV2.py
+++ versions/01_help_guiV2.py
@@ -1,10 +1,8 @@
 from tkinter import *
-# import random
 
 
 class Converter:
     def __init__(self):
-        print("hello world")
 
         # Formatting variables...
         background_color = "light blue"
@@ -12,15 +10,28 @@ class Converter:
         # Converter main screen GUI...
         self.converter_frame = Frame(width=300,
                                      height=300,
-                                     bg=background_color)
+                                     bg=background_color, pady=10)
         self.converter_frame.grid()
 
         # Temperature conversion Heading (row 0)
-        self.temp_converter_label = Label(text="Temperature Converter",
+        self.temp_converter_label = Label(self.converter_frame,
+                                          text="Temperature Converter",
                                           font=("Arial", "16", "bold"),
                                           bg=background_color,
                                           padx=10, pady=10)
         self.temp_converter_label.grid(row=0)
+
+        # Help button (row 1)
+        self.help_button = Button(self.converter_frame, text="Help",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10,
+                                  command=self.help)
+        self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = Help()
+        get_help.help_text.configure(text="Help text goes here")
 
 
 # main routine
